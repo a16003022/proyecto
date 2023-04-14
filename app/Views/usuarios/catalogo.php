af065ff07fbdd89f7a892345ee281dd8f8440441
HeislerNarvaez <a16003022@alumnos.uady.mx>
2023-04-14 13:07:43 -0600

    subiendo inventario

--- app/Views/usuarios/catalogo.php
+++ app/Views/usuarios/catalogo.php
@@ -5,10 +5,6 @@
   
   }
 
-  input {
-  display: none;
-  }
-
   .container-corazon input {
   position: absolute;
   opacity: 0;
@@ -101,37 +97,8 @@
                 </div>
                 <div class=" row card-footer" style="border: none; background-color: transparent;">
 
-                  <div class="col-6">
-                    <form action="<?php echo base_url('/catalogoPlayeras'); ?>" method="POST"> 
-                      <input id="idProducto" name="idProducto" type="hidden" value=<?php echo $dat['idProducto']?>>
-                      <input id="nombre" name="nombre" type="hidden" value="<?php echo $dat['nombre']?>">
-                      <input id="precio" name="precio" type="hidden" value="<?php echo $dat['precio']?>">
-                      <input id="stock" name="stock" type="hidden" value="<?php echo $dat['cantidad']-1?>">
-                      <input id="cantidad" name="cantidad" type="hidden" value="1">
-                      <?php $prodAñadido = false; 
-                        foreach($carrito as $prod):
-                          if ($prod["idProducto"] == $dat["idProducto"]){
-                            $prodAñadido = true;
-                            break;
-                          }
-                        endforeach;
-                        if ($prodAñadido){ ?>
-                        <button type="submit" class="btn btn-primary btn-jumbotron" disabled>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-check-fill" viewBox="0 0 16 16">
-                              <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zm-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
-                            </svg>
-                          </button>
-                        <?php }else{ ?>
-                        <button type="submit" class="btn btn-primary btn-jumbotron">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
-                            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-                          </svg> Añadir
-                        </button>  
-                        <?php } ?>
-                    </form>	
-                  </div>
-                  <div class="col-6">
-                  <form action="<?php echo base_url('/agregarLista'); ?>" method="POST" style="float:right;">
+                  <div class="col-4">
+                  <form action="<?php echo base_url('/agregarLista'); ?>" method="POST">
                       <input id="idProducto" name="idProducto" type="hidden" value=<?php echo $dat['idProducto']?>>
                       <input id="nombre" name="nombre" type="hidden" value="<?php echo $dat['nombre']?>">
                       <input id="precio" name="precio" type="hidden" value="<?php echo $dat['precio']?>">
@@ -163,6 +130,35 @@
                           </label>
                         <?php } ?>
                   </form> 
+                  </div>
+                  <div class="col-8">
+                    <form action="<?php echo base_url('/catalogoPlayeras'); ?>" method="POST" style="float:right;"> 
+                      <input id="idProducto" name="idProducto" type="hidden" value=<?php echo $dat['idProducto']?>>
+                      <input id="nombre" name="nombre" type="hidden" value="<?php echo $dat['nombre']?>">
+                      <input id="precio" name="precio" type="hidden" value="<?php echo $dat['precio']?>">
+                      <input id="stock" name="stock" type="hidden" value="<?php echo $dat['cantidad']?>">
+                      <?php $prodAñadido = false; 
+                        foreach($carrito as $prod):
+                          if ($prod["idProducto"] == $dat["idProducto"]){
+                            $prodAñadido = true;
+                            break;
+                          }
+                        endforeach;
+                        if ($prodAñadido){ ?>
+                        <button type="submit" class="btn btn-primary btn-jumbotron" disabled>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-check-fill" viewBox="0 0 16 16">
+                              <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zm-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+                            </svg> Añadido
+                          </button>
+                        <?php }else{ ?>
+                        <button type="submit" class="btn btn-primary btn-jumbotron">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                          </svg>
+                        </button>
+                        <input id="cantidad" name="cantidad" type="number" min="1" max="<?php echo $dat['cantidad']?>" value="1" onkeydown="return false;">  
+                        <?php } ?>
+                    </form>	
                   </div>                  
                 </div>
               </div>
@@ -173,3 +169,25 @@
     </div>
   </section>
 </body>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+  $(document).ready(function() {
+    // Obtenemos el valor inicial de la cantidad
+    var cantidad = $("#cantidad").val();
+    // Calculamos el stock inicial restando la cantidad
+    var stock = <?php echo $dat['cantidad'] - $cantidad ?>;
+    // Actualizamos el valor del input con el stock inicial
+    $("#stock").val(stock);
+    
+    // Añadimos un listener al input con id=cantidad
+    $("#cantidad").on("change", function() {
+      // Obtenemos el nuevo valor de la cantidad
+      cantidad = $(this).val();
+      // Calculamos el nuevo valor del stock restando la cantidad
+      stock = <?php echo $dat['cantidad'] ?> - cantidad;
+      // Actualizamos el valor del input con el nuevo stock
+      $("#stock").val(stock);
+    });
+  });
+</script>
